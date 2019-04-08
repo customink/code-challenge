e098026859721736ab6af1a2e6a6b95f9962ab2f
Matúš Duchoň <matus.duchon@gmail.com>
2019-04-08 10:18:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const inputPaths = [
+  '---RRURDR-',
+  'DRDR--RRDDD-',
+]
+
+function completePath(path) {
+  return path
+}
+
+inputPaths
+  .map(completePath)
+  .forEach(path => console.log(path))
